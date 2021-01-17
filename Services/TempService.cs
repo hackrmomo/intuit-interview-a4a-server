e0345b90c59e9cab421c5c6568c7ff90b420f7e3
Mohammad Al-Ahdal <mo@mohammad.dev>
2021-01-17 08:38:06 -0700

    remove uneeded database

--- Services/TempService.cs
+++ Services/TempService.cs
@@ -1,5 +1,4 @@
 using System.Threading.Tasks;
-using MySql.Data.MySqlClient;
 using Microsoft.Extensions.Configuration;
 
 namespace Bolt.Services
@@ -11,18 +10,11 @@ namespace Bolt.Services
 
     public class TempService : ITempService
     {
-        private MySqlConnection _conn;
         public TempService(IConfiguration config)
         {
-            _conn = new MySqlConnection(config.GetSection("MySQLConnectionString").Value);
         }
 
         public async Task<string> BasicOperation () {
-            using (_conn) {
-                await _conn.OpenAsync();
-                var command = new MySqlCommand("SELECT 1", _conn);
-                var result = await command.ExecuteScalarAsync();
-            }
             return "Bolt Services Active";
         }
     }
