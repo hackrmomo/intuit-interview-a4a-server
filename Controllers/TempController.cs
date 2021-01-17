fe21c7ddcebef71eb1098399c9a580be2e0b4c3d
Mohammad Al-Ahdal <mo@mohammad.dev>
2021-01-17 07:33:48 -0700

    fix namespace to Bolt

--- Controllers/TempController.cs
+++ Controllers/TempController.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-namespace MavenlinkUnify.WebAdmin.Controllers
+namespace Bolt.Controllers
 {
     [ApiController]
     [Route("temp")]
