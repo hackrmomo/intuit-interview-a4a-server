d405be58637e87e48e41e23c8c5b2d5a297dac9c
Mohammad Al-Ahdal <mo@mohammad.dev>
2021-01-19 09:02:59 -0700

    move currency conversion to service

--- Models/ITransactionEntry.cs
+++ Models/ITransactionEntry.cs
@@ -1,7 +1,9 @@
+using System;
 namespace Bolt.Models
 {
     public class ITransactionEntry
     {
+        public Guid id { get; set; }
         public string Account { get; set; }
         public string Type { get; set; }
         public string AccountTerm { get; set; }
